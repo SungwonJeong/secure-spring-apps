770e767e0e78151f908c21de4d2416998d2c7090
sungwon <aogn07@naver.com>
2025-01-23 15:50:37 +0900

    base entity & jpa config

--- core/core-api/src/main/java/com/example/securespringapps/CoreApiApplication.java
+++ core/core-api/src/main/java/com/example/securespringapps/CoreApiApplication.java
@@ -2,8 +2,10 @@ package com.example.securespringapps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CoreApiApplication {
 
     public static void main(String[] args) {
---
+++ storage/db-core/src/main/java/com/example/securespringapps/storage/db/core/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.example.securespringapps.storage.db.core;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column
+    private LocalDateTime modifiedAt;
+}
